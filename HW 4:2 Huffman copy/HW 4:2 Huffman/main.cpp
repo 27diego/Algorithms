688a4ef90fb906d97f6d3bfa118a8e78da12df23
School <school@Diegos-MacBook-Pro.local>
2019-04-04 21:59:03 -0700

    :/

--- HW 4:2 Huffman copy/HW 4:2 Huffman/main.cpp
+++ HW 4:2 Huffman copy/HW 4:2 Huffman/main.cpp
@@ -31,7 +31,7 @@ private:
     void swap(int i, int j);
     void heapifyUp(int idx);
     void heapifyDown(int i);
-
+    
     
 public://declaring functions within class
     Node *extractMin();
@@ -112,28 +112,111 @@ int priorityHeap::size()
     return data.size();
 }
 
-struct node_cmp{
+struct tieBreaker
+{
     bool operator() (Node* a, Node* b)
     {
+        if(a->frequency==b->frequency)
+        {
+            if(a->letter=='$'&&b->letter=='$')
+            {
+                return a->frequency<b->frequency;
+            }
+            else if(a->letter=='$')
+            {
+                return b<a;
+            }
+            else if(b->letter=='$')
+            {
+                return a<b;
+            }
+            return a->letter<b->letter;
+        }
         return a->frequency<b->frequency;
     }
 };
 
+
+
 class HuffTree{
     Node* root;
+    priorityHeap pq;
 public:
     HuffTree()
     {
         root = nullptr;
     }
-    void insert(vector<Node*> letters)
+    void build(string sentence)
     {
-        std::priority_queue<Node*, std::vector<Node*>, node_cmp()> q;
-
-        for(int x=0;x<letters.size();x++)
+        unordered_map<char,int> letters;
+        unordered_map<char, int>::iterator crawler;
+        
+        for(int x=0;x<sentence.length();x++)
         {
-            q.push(letters[x]);
+            letters.insert(make_pair(sentence[x], 0));
         }
+        for(int x=0;x<sentence.length();x++)
+        {
+            for(crawler = letters.begin();crawler!=letters.end();crawler++)
+            {
+                if(sentence[x]==crawler->first)
+                {
+                    crawler->second++;
+                }
+            }
+        }
+        for(crawler = letters.begin();crawler!=letters.end();crawler++)
+        {
+            pq.insert(new Node(crawler->first,crawler->second));
+        }
+        
+        vector<Node*>priorityletters;
+        
+        while(pq.size()!=0)
+        {
+            priorityletters.push_back(pq.extractMin());
+        }
+        
+        queue<Node*>q;
+        
+        sort(priorityletters.begin(), priorityletters.end(), tieBreaker());
+        
+        Node* temp = new Node('$',0);
+        while(!priorityletters.empty())//trying to push $ to end to push to final queue
+        {
+            if(priorityletters.size()==1)
+            {
+                break;
+            }
+            temp=new Node('$',0);
+            temp->left = priorityletters[0];
+            priorityletters.erase(priorityletters.begin());
+            if(priorityletters[0]!=nullptr)
+            {
+                temp->right = priorityletters[0];
+                priorityletters.erase(priorityletters.begin());
+            }
+            temp->frequency = temp->right->frequency + temp->left->frequency;
+            priorityletters.push_back(temp);
+            sort(priorityletters.begin(), priorityletters.end(), tieBreaker());
+            
+            //            cout<<"Size: "<<priorityletters.size()<<endl;
+            //            for(int x=0;x<priorityletters.size();x++)
+            //            {
+            //                cout<<"char: "<<priorityletters[x]->letter<<":"<<priorityletters[x]->frequency<<endl;
+            //            }
+            //            cout<<endl<<endl;
+        }
+        
+        root = priorityletters[0];
+        priorityletters.erase(priorityletters.begin());
+        
+        //        cout<<"root: "<<root->letter<<":"<<root->frequency<<endl;
+        
+    }
+    
+    void createMap()
+    {
         
     }
     
@@ -142,47 +225,38 @@ public:
 
 
 int main() {
-    priorityHeap pq;
-//    pq.insert(new Node('A', 4));
-//    pq.insert(new Node('B', 2));
-//    pq.insert(new Node('C', 1));
-//    pq.insert(new Node('D', 7));
-//    pq.insert(new Node('E', 7));
-//    pq.insert(new Node('F', 3));
-//    pq.insert(new Node('O', 1));
-//    pq.insert(new Node('S', 2));
-//    pq.insert(new Node('T', 1));
-//    pq.insert(new Node('V', 1));
-//    pq.insert(new Node(' ', 7));
+    HuffTree tree;
     
     string sentence = "i love computer science";
-    unordered_map<char,int> letters;
-    unordered_map<char, int>::iterator crawler;
-
-    for(int x=0;x<sentence.length();x++)
-    {
-        letters.insert(make_pair(sentence[x], 0));
-    }
-    for(int x=0;x<sentence.length();x++)
-    {
-        for(crawler = letters.begin();crawler!=letters.end();crawler++)
-        {
-            if(sentence[x]==crawler->first)
-            {
-                crawler->second++;
-            }
-        }
-    }
-    for(crawler = letters.begin();crawler!=letters.end();crawler++)
-    {
-        pq.insert(new Node(crawler->first,crawler->second));
-    }
     
+    tree.build(sentence);
     
-    
-   pq.print();
 }
 
 /*
  1100101110101100110111111011100011010101101100100111
+ */
+
+
+
+/*
+ void printPostHelp()
+ {
+ postOrder(root);
+ }
+ 
+ 
+ void postOrder(Node* root) //finished
+ {
+ if (root) {
+ postOrder(root->left);
+ printNode(root);
+ postOrder(root->right);
+ }
+ }
+ 
+ void printNode(Node* root)//finished
+ {
+ cout << root->data<<" ";
+ 
  */
